133f8cbb1e87a3e37a0f053fd54f2bcd4d88c4a0
VegardIversen <vegard-si@hotmail.com>
2021-10-19 16:34:12 +0200

    done with 2ab, 2c under developement

--- assignment1/task2ab.py
+++ assignment1/task2ab.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import pathlib
 from utils import read_im, save_im
+import numpy as np
 output_dir = pathlib.Path("image_solutions")
 output_dir.mkdir(exist_ok=True)
+
 
 
 im = read_im(pathlib.Path("images", "lake.jpg"))
@@ -18,6 +20,9 @@ def greyscale(im):
     Returns:
         im ([type]): [np.array of shape [H, W]]
     """
+    grey = np.array([ 0.212, 0.7152, 0.0722]) #gray from the assignment
+    #here I dont need to have the slicing but its nice to have if there is a RGBa image.
+    im = np.dot(im[:,:,:3],grey)
 
     return im
 
@@ -25,6 +30,7 @@ def greyscale(im):
 im_greyscale = greyscale(im)
 save_im(output_dir.joinpath("lake_greyscale.jpg"), im_greyscale, cmap="gray")
 plt.imshow(im_greyscale, cmap="gray")
+#plt.show()
 
 
 def inverse(im):
@@ -36,5 +42,10 @@ def inverse(im):
     Returns:
         im ([type]): [np.array of shape [H, W]]
     """
-    # YOUR CODE HERE
+    im = -im + 255
     return im
+
+im_greyscale_inverse = inverse(im_greyscale)
+save_im(output_dir.joinpath("lake_greyscale_inverse.jpg"), im_greyscale, cmap="gray")
+plt.imshow(im_greyscale_inverse, cmap="gray")
+#plt.show()
