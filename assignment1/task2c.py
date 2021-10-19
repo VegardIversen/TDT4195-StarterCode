ed6523781037ae479a30863292af0bbf6a60a1d8
VegardIversen <vegard-si@hotmail.com>
2021-10-19 16:34:12 +0200

    done with 2ab, 2c under developement

--- assignment1/task2c.py
+++ assignment1/task2c.py
@@ -9,6 +9,15 @@ output_dir.mkdir(exist_ok=True)
 im = read_im(pathlib.Path("images", "lake.jpg"))
 plt.imshow(im)
 
+#P = ((S-1)*W-S+F)/2#, with F = filter size, S = stride, W = input size#calculating padding for no odd number and with a stride 
+#but here we assume odd number and stride =1 we get that the padding size is P= (F-1)/2
+def pad(img, h, w):
+    #  in case when you have odd number
+    top_pad = np.floor((h - img.shape[0]) / 2).astype(np.uint16)
+    bottom_pad = np.ceil((h - img.shape[0]) / 2).astype(np.uint16)
+    right_pad = np.ceil((w - img.shape[1]) / 2).astype(np.uint16)
+    left_pad = np.floor((w - img.shape[1]) / 2).astype(np.uint16)
+    return np.copy(np.pad(img, ((top_pad, bottom_pad), (left_pad, right_pad), (0, 0)), mode='constant', constant_values=0))
 
 def convolve_im(im, kernel,
                 ):
@@ -21,9 +30,32 @@ def convolve_im(im, kernel,
     Returns:
         [type]: [np.array of shape [H, W, 3]. should be same as im]
     """
-    assert len(im.shape) == 3
 
-    return im
+    assert len(im.shape) == 3
+    image_height = im.shape[0]
+    image_width = im.shape[1]
+    image_depth = im.shape[2]
+    #since the kernel is a KxK matrix we only need one of the sizes
+    kernel_size = kernel.shape[0]
+    #finding the size of the padding
+    pad_size = (kernel_size-1)//2
+    #creating a empty array with padding
+    out_im = np.zeros(shape=(image_height+2*pad_size,image_width+2*pad_size,image_depth)) #this seems to pad it correctly when i show it
+    image_height_pad = out_im.shape[0]
+    image_width_pad = out_im.shape[1]
+    #fitting the image in the middle, and now I got padding
+    out_im[pad_size:-pad_size,pad_size:-pad_size,:] = im
+    print(out_im.shape)
+    print('----------------------')
+    kernel_new_size = np.array([kernel]*image_depth)
+    im_pad = pad(im,image_height,image_width)
+    for y in range(pad_size,image_height_pad-pad_size):
+
+        for x in range(pad_size,image_width_pad-pad_size):
+            out_im[y-kernel_size:y+kernel_size,x-kernel_size:x+kernel_size,:] *= kernel_new_size
+
+    
+    #return im
 
 
 if __name__ == "__main__":
