64904b8c35be01494cf8f7f1641818d858d08390
VegardIversen <vegard-si@hotmail.com>
2021-10-27 11:53:33 +0200

    finishing up task2c and adding comments

--- assignment1/task2c.py
+++ assignment1/task2c.py
@@ -3,7 +3,11 @@ import pathlib
 import numpy as np
 from utils import read_im, save_im, normalize
 import time
-start_time = time.time()
+'''
+I tried to do this with multiplying 3 dimensions at the same time, but this was around 50% slower than doing this.
+'''
+
+#start_time = time.time()
 output_dir = pathlib.Path("image_solutions")
 output_dir.mkdir(exist_ok=True)
 
@@ -14,31 +18,12 @@ plt.imshow(im)
 #P = ((S-1)*W-S+F)/2#, with F = filter size, S = stride, W = input size#calculating padding for no odd number and with a stride 
 #but here we assume odd number and stride =1 we get that the padding size is P= (F-1)/2
 def pad(img, h, w):
-    #  in case when you have odd number
+    #  in case when you have even number
     top_pad = np.floor((h - img.shape[0]) / 2).astype(np.uint16)
     bottom_pad = np.ceil((h - img.shape[0]) / 2).astype(np.uint16)
     right_pad = np.ceil((w - img.shape[1]) / 2).astype(np.uint16)
     left_pad = np.floor((w - img.shape[1]) / 2).astype(np.uint16)
     return np.copy(np.pad(img, ((top_pad, bottom_pad), (left_pad, right_pad), (0, 0)), mode='constant', constant_values=0))
-
-def convole2D_kxk(matrix, kernel):
-    matrix_size = matrix.shape[0]
-    #matrix_depth = matrix.shape[2]
-    #mat = np.zeros(shape=(matrix_size,matrix_size,matrix_depth))
-    pix_val = 0
-
-    #print('---------------matrise----------------')
-    #print(matrix)
-    for i in range(matrix_size):
-        for j in range(matrix_size):
-            #print(f'i: {i}, j: {j}')
-            pix_val += matrix[j,i]*kernel[j,i]
-            #print(pix_val)
-    #print('---------------ny matrise---------------')
-    #print(mat)
-
-    return pix_val
-
 
 def convolve_im(im, kernel,
                 ):
@@ -53,51 +38,40 @@ def convolve_im(im, kernel,
     """
 
     assert len(im.shape) == 3
+    #Fetching the shape of the image
     image_height = im.shape[0]
     image_width = im.shape[1]
     image_depth = im.shape[2]
-    #since the kernel is a KxK matrix we only need one of the sizes
+    #Fetching kernel shape
+    #Since the kernel is a KxK matrix we only need one of the sizes
     kernel_size = kernel.shape[0]
-    #finding the size of the padding
-    pad_size = (kernel_size-1)//2
-    kernel_flip = np.flip(kernel)
-    #creating a empty array with padding
-    out_im = np.zeros(shape=(image_height+2*pad_size,image_width+2*pad_size,image_depth)) #this seems to pad it correctly when i show it
-    im_conv = np.zeros(shape=(image_height+2*pad_size,image_width+2*pad_size,image_depth))
-    image_height_pad = out_im.shape[0]
-    image_width_pad = out_im.shape[1]
-    #fitting the image in the middle, and now I got padding
-    out_im[pad_size:-pad_size,pad_size:-pad_size,:] = im
-    print(out_im.shape)
-    print('----------------------')
-  
-    
-    #kernel_new_size = np.tile(kernel[:,:,None],(1,1,image_depth))
-    #print(im[0:1,0:4,0:4])
-    #print(kernel_new_size[0:1,0:2,0:2].shape)
-    #print(kernel_new_size)
-    
-    #print(np.flip(kernel_new_size).shape)
-    #im_pad = pad(im,image_height,image_width)
-    for d in range(image_depth):
-        for y in range(pad_size,image_height_pad-pad_size):
-            for x in range(pad_size,image_width_pad-pad_size):
-                pix_val = 0.0
-                for n in range(kernel.shape[0]):
-                    for k in range(kernel.shape[0]):
-                        pix_val += out_im[x-pad_size + n,y-pad_size + k,d]*kernel_flip[n,k]
-                im_conv[x,y,d] = pix_val
-        
-    
-                #out_im[x,y,d] = convole2D_kxk(out_im[x-pad_size:x+pad_size+1,y-pad_size:y+pad_size+1,d],kernel)
 
-                
-    
-    im = im_conv[pad_size:-pad_size,pad_size:-pad_size,:]
-    #im = im/np.amax(im)
-    #im = np.clip(im,0,1)
-    #plt.imshow(out_im[pad_size:-pad_size,pad_size:-pad_size,:])
-    
+    #Finding the size of the padding
+    pad_size = (kernel_size-1)//2
+
+    #Flipping the kernel so that we convolve and not correlate
+    kernel_flip = np.flip(kernel)
+
+    #Creating a empty array with padding and a copy to be safe
+    im_conv = np.zeros(shape=(image_height+2*pad_size,image_width+2*pad_size,image_depth))
+    im_pad_copy = np.zeros(shape=(image_height+2*pad_size,image_width+2*pad_size,image_depth)).astype(float)
+    image_width_pad = im_pad_copy.shape[0]
+    image_height_pad = im_pad_copy.shape[1]
+    #Fitting the image in the middle, and now I got padding
+    im_pad_copy[pad_size:-pad_size,pad_size:-pad_size,:] = im
+
+    print(im_pad_copy.shape)
+    print('----------------------')
+    for d in range(image_depth): #Image depth (color, RGB)
+        for y in range(pad_size,image_height_pad-pad_size): #heigth of the image
+            for x in range(pad_size,image_width_pad-pad_size): # width of the image
+                pix_val = 0.0 #setting the pixel value
+                for n in range(kernel.shape[0]): # iterating through the kernel
+                    for k in range(kernel.shape[0]):
+                        pix_val += im_pad_copy[x-pad_size + n,y-pad_size + k,d]*kernel_flip[n,k] #summing the multiplications
+                im_conv[x,y,d] = pix_val #setting the pixel value to the pixel in the convolve image
+
+    im = im_conv[pad_size:-pad_size,pad_size:-pad_size,:] #removing padding
     return im
 
 
@@ -118,9 +92,9 @@ if __name__ == "__main__":
 
     # Convolve images
     im_smoothed = convolve_im(im.copy(), h_b)
-    save_im(output_dir.joinpath("im_smoothed_time.jpg"), im_smoothed)
+    save_im(output_dir.joinpath("im_smoothed.jpg"), im_smoothed)
     im_sobel = convolve_im(im, sobel_x)
-    save_im(output_dir.joinpath("im_sobel_time.jpg"), im_sobel)
+    save_im(output_dir.joinpath("im_sobel.jpg"), im_sobel)
 
     # DO NOT CHANGE. Checking that your function returns as expected
     assert isinstance(
@@ -135,4 +109,4 @@ if __name__ == "__main__":
     plt.subplot(1, 2, 2)
     plt.imshow(normalize(im_sobel))
     plt.show()
-print("--- %s seconds ---" % (time.time() - start_time))
+#print("--- %s seconds ---" % (time.time() - start_time))
